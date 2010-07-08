887bd335f56f2052adc082d4c2db8731425f7c13
Francisco Souza <francisco@franciscosouza.net>
2010-07-08 19:54:25 -0300

    Changed terrain to the root url works properly

--- terrain.py
+++ terrain.py
@@ -4,7 +4,7 @@ from selenium import get_driver, FIREFOX
 @before.all
 def setup_browser():
     world.browser = get_driver(FIREFOX)
-    world.root_url = 'http://localhost/'
+    world.root_url = 'http://localhost/php_app_test/'
 
 @after.all
 def teardown_browser():
